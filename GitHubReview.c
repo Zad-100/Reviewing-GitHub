e7a9fd81a15f2da528e6f482e614315739c0cdaa
Zad-100 <akash2001parida@gmail.com>
2021-02-10 20:52:53 +0530

    Successful in understanding the working of the git pull and git push commands.

--- GitHubReview.c
+++ GitHubReview.c
@@ -4,4 +4,5 @@ int main( void )
 {
    printf( "%s", "\nHello, there! This is a C file made as a part of reviewing of Git Hub session.\n" );
    printf( "%s", "\nYou are going really great, Akash!\nJust don't stop!\n" );
+   printf( "%s", "\nSuccessfully ran the tests on git push and pull commands.\n" );
 }
